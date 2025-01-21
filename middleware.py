fa1f67d752cb31494303ad50a6b01ef9d25bc5a5
Qazafi-Hussain-Developer <qazafihussainx@gmail.com>
2025-01-21 09:31:04 +0500

    docs: add API documentation

--- middleware.py
+++ middleware.py
@@ -7,3 +7,4 @@
 # 2025-01-16 22:44 - refactor: extract reusable components
 # 2025-01-16 15:25 - perf: improve load time by 50%
 # 2025-01-20 22:59 - test: write integration tests
+# 2025-01-21 09:31 - refactor: clean up API handlers

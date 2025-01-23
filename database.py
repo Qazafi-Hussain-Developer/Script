fbfc54465650cf4195d97590d64b0d7438886cfd
Qazafi-Hussain-Developer <qazafihussainx@gmail.com>
2025-01-23 17:52:26 +0500

    test: write integration tests

--- database.py
+++ database.py
@@ -3,3 +3,4 @@
 # 2025-01-10 12:05 - fix: correct pagination bug
 # 2025-01-16 21:13 - test: add unit tests for auth service
 # 2025-01-16 12:11 - perf: reduce bundle size by 30%
+# 2025-01-23 17:52 - perf: optimize database queries

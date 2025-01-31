7d6ee1f57155dd89d6fedd1e11354514f986b1e1
Qazafi-Hussain-Developer <qazafihussainx@gmail.com>
2025-01-31 17:29:55 +0500

    fix: correct pagination bug

--- database.py
+++ database.py
@@ -4,3 +4,4 @@
 # 2025-01-16 21:13 - test: add unit tests for auth service
 # 2025-01-16 12:11 - perf: reduce bundle size by 30%
 # 2025-01-23 17:52 - perf: optimize database queries
+# 2025-01-31 17:29 - test: add unit tests for auth service

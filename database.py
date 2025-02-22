899a92413c0f776bd1488fbf68a016492271e217
Qazafi-Hussain-Developer <qazafihussainx@gmail.com>
2025-02-22 09:51:52 +0500

    feat: add search functionality

--- database.py
+++ database.py
@@ -5,3 +5,4 @@
 # 2025-01-16 12:11 - perf: reduce bundle size by 30%
 # 2025-01-23 17:52 - perf: optimize database queries
 # 2025-01-31 17:29 - test: add unit tests for auth service
+# 2025-02-22 09:51 - test: add unit tests for auth service

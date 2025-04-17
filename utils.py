ffbdf701a316e1974961e2b00d8e96bf52ec4c04
Qazafi-Hussain-Developer <qazafihussainx@gmail.com>
2025-04-17 20:09:50 +0500

    feat: add payment gateway integration

--- utils.py
+++ utils.py
@@ -343,3 +343,4 @@
 # 2025-03-13 16:15 - test: add unit tests for auth service
 # 2025-03-24 09:59 - chore: update dependencies to latest
 # 2025-04-04 11:48 - fix: correct CSS responsive layout
+# 2025-04-17 20:09 - feat: implement email notifications

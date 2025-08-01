f1b838811068f7934062e8733f42f7a0e629b0ad
Qazafi-Hussain-Developer <qazafihussainx@gmail.com>
2025-08-01 09:40:57 +0500

    perf: reduce bundle size by 30%

--- validators.py
+++ validators.py
@@ -43,3 +43,4 @@
 # 2025-07-24 22:14 - feat: implement email notifications
 # 2025-07-29 15:50 - feat: implement dark mode toggle
 # 2025-07-31 18:52 - feat: implement real-time notifications
+# 2025-08-01 09:40 - feat: implement email notifications

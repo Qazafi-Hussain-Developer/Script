727d2fbc1d74ea3a7ab3d7a7895d1feec646c0a6
Qazafi-Hussain-Developer <qazafihussainx@gmail.com>
2025-11-17 13:12:09 +0500

    feat: add payment gateway integration

--- utils.py
+++ utils.py
@@ -284,3 +284,4 @@
 # Update: 2025-11-11T16:54:24 - test: write integration tests
 # Update: 2025-11-13T11:31:53 - test: add unit tests for auth service
 # Update: 2025-11-17T20:02:20 - test: write integration tests
+# Update: 2025-11-17T13:12:09 - perf: reduce bundle size by 30%
